00e533a13980e46ef4cc8861fe27622b41ca5448
Michael Still <mikal@stillhq.com>
2016-12-28 13:27:02 +1100

    Move keystonemiddleware config into config file.

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ LOG = logging.getLogger(__name__)
 
 @wsgify
 def application(req):
+    LOG.debug('Application ran')
     return Response('Hello World')
 
 
@@ -29,9 +30,19 @@ def app_factory(global_config, **local_config):
 
 def main():
     logging.register_options(CONF)
+
+    # Make keystonemiddleware emit debug logs
+    extra_default_log_levels = ['keystonemiddleware=DEBUG']
+    logging.set_defaults(default_log_levels=(logging.get_default_log_levels() +
+                                             extra_default_log_levels))
+
+    # Parse our config
     CONF(sys.argv[1:])
+
+    # Set us up to log as well
     logging.setup(CONF, 'vendordata')
 
+    # Start the web server
     wsgi_app = loadapp('config:paste.ini', relative_to='.')
     httpserver.serve(wsgi_app, host='127.0.0.1', port=8080)
 
